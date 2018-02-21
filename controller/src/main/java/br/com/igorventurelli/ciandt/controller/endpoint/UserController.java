13cda66a149fc3a4aaddbd5d520f3e2c872c3b13
Igor Venturelli <igor.venturelli@me.com>
2018-02-21 16:13:15 -0300

    Removing unused imports

--- controller/src/main/java/br/com/igorventurelli/ciandt/controller/endpoint/UserController.java
+++ controller/src/main/java/br/com/igorventurelli/ciandt/controller/endpoint/UserController.java
@@ -1,7 +1,6 @@
 package br.com.igorventurelli.ciandt.controller.endpoint;
 
 import br.com.igorventurelli.ciandt.controller.representation.UserRepresentation;
-import br.com.igorventurelli.ciandt.dto.dto.UserDTO;
 import br.com.igorventurelli.ciandt.service.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
