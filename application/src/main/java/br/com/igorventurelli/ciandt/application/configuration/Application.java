bd70c5389f3f3cd56d656de158a0e7ff9cf465cb
Igor Venturelli <igor.venturelli@me.com>
2018-02-21 17:07:01 -0300

    Removing unused imports

--- application/src/main/java/br/com/igorventurelli/ciandt/application/configuration/Application.java
+++ application/src/main/java/br/com/igorventurelli/ciandt/application/configuration/Application.java
@@ -3,7 +3,6 @@ package br.com.igorventurelli.ciandt.application.configuration;
 import br.com.igorventurelli.ciandt.controller.configuration.ControllerModuleConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
