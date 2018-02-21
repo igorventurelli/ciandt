584820128fc5dbfc774603537a57bce4b63766c0
Igor Venturelli <igor.venturelli@me.com>
2018-02-21 15:42:42 -0300

    @Data replaced by @Getter and @Setter

--- controller/src/main/java/br/com/igorventurelli/ciandt/controller/representation/UserRepresentation.java
+++ controller/src/main/java/br/com/igorventurelli/ciandt/controller/representation/UserRepresentation.java
@@ -1,11 +1,10 @@
 package br.com.igorventurelli.ciandt.controller.representation;
 
 import br.com.igorventurelli.ciandt.dto.dto.UserDTO;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserRepresentation {
